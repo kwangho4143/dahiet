7af2d0419552ac7b2a107724a47c5f944b75c481
AO <AO@DESKTOP-JDQNDEU>
2020-10-29 17:49:42 +0900

    1029_C_3

--- dahiet/src/main/webapp/forms/contact.php
+++ dahiet/src/main/webapp/forms/contact.php
@@ -11,9 +11,9 @@
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
-  } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
+  } else {
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
