3c2ecbf439e38a24827e6a815b5134ac5111bc77
duavudrkd123 <ypgcute@gmail.com>
2020-11-02 21:55:04 +0900

    1102_C_04

---
+++ dahiet/src/main/java/com/dahiet/vo/ItemCodeVO.java
@@ -0,0 +1,32 @@
+package com.dahiet.vo;
+
+public class ItemCodeVO {
+	private String itemname; // 상세 지역 코드
+	private String itemid; // 지역 코드
+	private String groupid; // 지역 한글 이름
+
+	public String getItemname() {
+		return itemname;
+	}
+
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+
+	public String getItemid() {
+		return itemid;
+	}
+
+	public void setItemid(String itemid) {
+		this.itemid = itemid;
+	}
+
+	public String getGroupid() {
+		return groupid;
+	}
+
+	public void setGroupid(String groupid) {
+		this.groupid = groupid;
+	}
+
+}
