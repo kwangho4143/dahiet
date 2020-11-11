1c97648a4cafeb0115c5fb11808ee15267619d3f
Dahee <hoihee00@naver.com>
2020-11-11 10:52:50 +0900

    s

--- dahiet/src/main/java/com/dahiet/dao/ResumeDetailDao.java
+++ dahiet/src/main/java/com/dahiet/dao/ResumeDetailDao.java
@@ -1,10 +1,54 @@
 package com.dahiet.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.dahiet.vo.ResumeVO;
 
-public class ResumeDetailDao {
+public class ResumeDetailDao extends DAO{
+	
+	private PreparedStatement psmt; // sql명령어 작성시에 사용
+	private ResultSet rs; // select 후 결과셋 받기
+	private ResumeVO vo;
+	
+	private final String LOADINFO = "SELECT * FROM USERS WHERE TEL = ?";
+	private final String loadresume = "";
+	private final String insertCareer = "";
+	private final String insertLicense = "";
+	private final String insertActivity = "";
+	private final String insertAward = "";
+	private final String insertLanguage = "";
+	
+	// user 테이블의 값 이력서에 불러오기
+	public List<ResumeVO> loadInfo(ResumeVO vo) {
+		List<ResumeVO> list = new ArrayList<>();
+		try {
+			psmt = conn.prepareStatement(LOADINFO);
+			psmt.setString(1, vo.getTel());
+			rs = psmt.executeQuery();
+			while(rs.next()) {
+				ResumeVO vo1=new ResumeVO();
+				vo1.setId(rs.getString("id"));
+				vo1.setName(rs.getString("name"));
+				vo1.setImag(rs.getString("imag"));
+				vo1.setBirth(rs.getDate("birth"));
+				vo1.setEmail(rs.getString("email"));
+				vo1.setTel(rs.getString("tel"));
+				vo1.setAddr(rs.getString("addr"));
+				vo1.setUniv(rs.getString("univ"));
+				vo1.setMajor(rs.getString("major"));
+				vo1.setScore(rs.getString("score"));
+				list.add(vo1);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		} return list;
+	}
 
 	public List<ResumeVO> insertCareer(String resume_seq) {
 		// TODO Auto-generated method stub
@@ -31,6 +75,27 @@ public class ResumeDetailDao {
 		return null;
 	}
 
-
+	
+	
+	private void close() {
+		try {
+			if(rs != null) {
+				rs.close();
+				System.out.println("rs 종료");
+			}
+			if(psmt != null) {
+				psmt.close();
+				System.out.println("psmt 종료");
+			}
+			if(conn != null) {
+				conn.close();
+				System.out.println("conn 종료");
+				System.out.println("--------------");
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
 
 }
