4cc1a454f243e60b6393eb59f83dac13c6d97b0c
lovetome90@gmail.com <lovetome90@gmail.com@LAPTOP-6REGLH3L>
2020-11-11 17:55:09 +0900

    정은

--- dahiet/src/main/java/com/dahiet/command/UpdateComInf.java
+++ dahiet/src/main/java/com/dahiet/command/UpdateComInf.java
@@ -1,10 +1,17 @@
 package com.dahiet.command;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import com.dahiet.common.Action;
+import com.dahiet.common.FileRenamePolicy;
+import com.dahiet.common.FileUtil;
 import com.dahiet.dao.ComDao;
 import com.dahiet.vo.ComVO;
 
@@ -28,6 +35,28 @@ public class UpdateComInf implements Action {
 		vo.setItem(request.getParameter("item"));
 		vo.setImg(request.getParameter("img"));
 		
+		String appPath = request.getServletContext().getRealPath("/images");
+		System.out.println(appPath);
+		// 여러건
+		try {
+			for (Part part : request.getParts()) { //파일 읽을 때
+				String fileName = FileUtil.extractFileName(part);
+				if (!fileName.equals("")) {
+					// 파일 명 중복 체크
+					String uploadFile = appPath + File.separator + fileName;
+					File renameFile = FileRenamePolicy.rename(new File(uploadFile));
+					part.write(renameFile.getAbsolutePath());
+					vo.setImg(renameFile.getName());
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//
 		int n = dao.ComUpdate(vo);
 		
 		request.setAttribute("n", n);
